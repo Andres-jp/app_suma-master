73a76f185ec953ef71223637c3bbb3f7c81913e5
AndresJP <alu0101416069@ull.edu.es>
2026-02-11 18:37:10 +0100

    Función resta

--- calc.py
+++ calc.py
@@ -3,3 +3,8 @@ def sum_two(a, b):
         return float(a) + float(b)
     except Exception as e:
         raise ValueError("Entradas no numéricas") from e
+def rest_two(a, b):
+    try:
+        return float(a) - float(b)
+    except Exception as e:
+        raise ValueError("Entradas no numéricas") from e
