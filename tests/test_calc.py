ac493e97b6850a45bf2911a32b9f7b89e864b8bc
AndresJP <alu0101416069@ull.edu.es>
2026-02-11 18:42:45 +0100

    test resta

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,5 @@
 import unittest
-from calc import sum_two
+from calc import rest_two, sum_two
 
 class TestCalc(unittest.TestCase):
     def test_sum(self):
@@ -10,5 +10,12 @@ class TestCalc(unittest.TestCase):
         with self.assertRaises(ValueError):
             sum_two("a", 3)
 
+    def test_rest(self):
+        self.assertEqual(rest_two(2, 3), -1.0)
+        self.assertAlmostEqual(rest_two("1.5", "2.5"), -1.0)
+
+    def test_error(self):
+        with self.assertRaises(ValueError):
+            rest_two("a", 3)
 if __name__ == '__main__':
     unittest.main()
